065810de693541e6033d491b4efb3cd107587010
Jesus <codeconqueror777@gmail.com>
2024-04-27 22:32:51 -0400

    Second Commit

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -35,6 +35,7 @@ export class Server {
         this.app.listen(this.port, () => {
 
             console.log(`Server running on port ${this.port}`)
+            console.log('New updated')
         })
     }
 }
